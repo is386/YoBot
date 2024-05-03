c38b489dd4b24d370a75200924302e3e69252996
is386 <singh.indervir89@gmail.com>
2024-05-02 20:59:18 -0400

    add waiting for reactions

--- schedule_cog.py
+++ schedule_cog.py
@@ -7,6 +7,9 @@ from zoneinfo import ZoneInfo
 
 time = datetime.time(hour=SCHEDULE_HOUR, minute=SCHEDULE_MINUTE, tzinfo=ZoneInfo(TZ_IDENTIFIER))
 
+def check_reaction(reaction, user):
+    return str(reaction.emoji) in [YO_EMOJI, NO_EMOJI]
+
 class ScheduleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -17,8 +20,22 @@ class ScheduleCog(commands.Cog):
 
     @tasks.loop(time=time)
     async def my_task(self):
-        embed = Embed(title="Yo or No?", color=0x006BB6)
         channel = self.bot.get_channel(CHANNEL_ID)
-        message = await channel.send(embed=embed)
+
+        message = await channel.send(embed=Embed(title="Yo or No?", color=0x006BB6))
         await message.add_reaction(YO_EMOJI)
         await message.add_reaction(NO_EMOJI)
+
+        reaction_count = 0
+        while True:
+            reaction, user = await self.bot.wait_for('reaction_add', check=check_reaction)
+            
+            if str(reaction.emoji) == NO_EMOJI:
+                await channel.send("@everyone thats gonna be a **NO** from me")
+                break
+            elif str(reaction.emoji) == YO_EMOJI:
+                reaction_count += 1
+
+            if reaction_count == 3:
+                await channel.send("@everyone **YO**, hop on voice chat")
+                break
